ebfcca9a80215570b341a4ff7b0197f79d16e639
rusmurs <myrsaloff@gmail.com>
2022-08-05 15:07:21 +0300

    step 2. adding format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,6 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('9.4.0')
- 
+  .version('9.4.0', '-V, --version', 'version number')
+  .option('-f, --format <type>', 'output format', 'stylish')
 program.parse();
