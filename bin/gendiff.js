3d6c5d643764cb1d04acf3194b6ae1fa80381c4b
Ruslan Mursalov <myrsaloff@gmail.com>
2022-08-15 19:35:21 +0300

    step 3-1. add fixtures

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,10 +2,14 @@
 
 import { Command } from 'commander';
 
+
 const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('9.4.0', '-V, --version', 'version number')
   .option('-f, --format <type>', 'output format', 'stylish')
-program.parse();
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)));
+
+program.parse(process.argv);
